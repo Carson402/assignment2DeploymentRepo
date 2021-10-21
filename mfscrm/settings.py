5f3635ed1bf997706b309eb44157434ea443c7a4
Carson <ccrockett@unomaha.edu>
2021-10-20 19:21:57 -0500

    Update settings.py
    turned debug = false and allowed pythonanywhere to be a host

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d(^#d0k+d#dg!$74#7(zn-nweu^zzwcus_1!i(&k6_)#q*uh=8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
